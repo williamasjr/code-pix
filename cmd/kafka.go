f0db203e762b54b7b12da51ce47e0048e36e9fb6
williamasjr <wasjr00@gmail.com>
2023-10-18 17:47:19 -0300

    feat:  kafka producer

---
+++ cmd/kafka.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/spf13/cobra"
+	"github.com/williamasjr/code-pix-go/application/kafka"
+)
+
+// kafkaCmd represents the kafka command
+var kafkaCmd = &cobra.Command{
+	Use:   "kafka",
+	Short: "Starting consuming transactions using apache kafka",
+	Run: func(cmd *cobra.Command, args []string) {
+		deliveryChan := make(chan ckafka.Event)
+		producer := kafka.NewKafkaProducer()
+
+		kafka.Publish("Ola kafka", "teste", producer, deliveryChan)
+		kafka.DeliveryReport(deliveryChan)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(kafkaCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// kafkaCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// kafkaCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
