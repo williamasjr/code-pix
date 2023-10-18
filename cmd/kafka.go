0a0f5938d7d3c2335958ab684ffb090c1f9eac3d
williamasjr <wasjr00@gmail.com>
2023-10-18 18:10:51 -0300

    feat: kafka process

--- cmd/kafka.go
+++ cmd/kafka.go
@@ -4,9 +4,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"os"
+
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/spf13/cobra"
 	"github.com/williamasjr/code-pix-go/application/kafka"
+	"github.com/williamasjr/code-pix-go/infra/db"
 )
 
 // kafkaCmd represents the kafka command
@@ -15,10 +18,14 @@ var kafkaCmd = &cobra.Command{
 	Short: "Starting consuming transactions using apache kafka",
 	Run: func(cmd *cobra.Command, args []string) {
 		deliveryChan := make(chan ckafka.Event)
+		database := db.ConnectDB(os.Getenv("env"))
 		producer := kafka.NewKafkaProducer()
 
-		kafka.Publish("Ola kafka", "teste", producer, deliveryChan)
-		kafka.DeliveryReport(deliveryChan)
+		kafka.Publish("Ola consumer", "teste", producer, deliveryChan)
+		go kafka.DeliveryReport(deliveryChan)
+
+		kafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
+		kafkaProcessor.Consume()
 	},
 }
 
