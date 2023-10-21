6ee79a6006150f3ce64bdf56eaa4881659884a0d
williamasjr <wasjr00@gmail.com>
2023-10-21 13:37:16 -0300

    feat: codepix refactor

---
+++ codepix/application/factory/factory.go
@@ -0,0 +1,19 @@
+package factory
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/williamasjr/codepix-go/application/usecase"
+	"github.com/williamasjr/codepix-go/infrastructure/repository"
+)
+
+func TransactionUseCaseFactory(database *gorm.DB) usecase.TransactionUseCase {
+	pixRepository := repository.PixKeyRepositoryDb{Db: database}
+	transactionRepository := repository.TransactionRepositoryDb{Db: database}
+
+	transactionUseCase := usecase.TransactionUseCase{
+		TransactionRepository: &transactionRepository,
+		PixRepository:         pixRepository,
+	}
+
+	return transactionUseCase
+}
